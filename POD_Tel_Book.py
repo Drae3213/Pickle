b3343a643fe6085e93dfae19e20488861a23ac05
Drae3213 <67166869+Drae3213@users.noreply.github.com>
2021-01-14 01:11:39 -0800

    added members print command line

--- POD_Tel_Book.py
+++ POD_Tel_Book.py
@@ -2,10 +2,10 @@ import pickle
 
 # 1. Leader Numbers
 leader = {}
-leader["Jacore"] = "(845) 200-6250"
-leader["Gabriel"] = "(510) 613-6288"
-leader["Aris"] = "(510) 229-6359"
-leader["Richard"] = "(510) 228-5623"
+leader["Jacore:"] = "(845) 200-6250"
+leader["Gabriel:"] = "(510) 613-6288"
+leader["Aris:"] = "(510) 229-6359"
+leader["Richard:"] = "(510) 228-5623"
 
 # 2. create/open pod_nbrs.pkl file
 pod_file = open("pod_nbrs.pkl", "wb")
@@ -15,9 +15,9 @@ pickle.dump(leader,pod_file)
 
 # 4. Member Numbers
 member = {}
-member["Glenn"] = "(510) 328-8290"
-member["Mallick"] = "(510) 409-8755"
-member["Ronin"] = "(415) 910-3415"
+member["Glenn:"] = "(510) 328-8290"
+member["Mallick:"] = "(510) 409-8755"
+member["Ronin:"] = "(415) 910-3415"
 
 # 5. Write Member numbers to pod_file
 pickle.dump(member,pod_file)
@@ -36,7 +36,9 @@ for key, value in leader.items():
 print("\n")
 
 # 9. Print POD members
-print("Members")
+print("Members:")
+for key, value in member.items():
+    print(key, value)
 
 print("\n")
 
